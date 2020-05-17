6a9614d5fec9c068d3f733a826dfb4f99c8dcc50
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-05-17 16:32:35 +0530

    add form

--- lib/src/screens/login_Page.dart
+++ lib/src/screens/login_Page.dart
@@ -7,12 +7,57 @@ class LoginPage extends StatefulWidget {
 }
 
 class LoginPageState extends State<LoginPage> {
+  emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email',
+        hintText: 'you@example.com',
+        icon: Icon(Icons.email),
+      ),
+    );
+  }
+
+  passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'password',
+        hintText: 'password',
+        icon: Icon(Icons.lock),
+      ),
+    );
+  }
+
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('login'),
       ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            emailField(),
+            passwordField(),
+            SizedBox(height: 10.0),
+            submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  submitButton() {
+    return RaisedButton(
+      onPressed: () {},
+      child: Text(
+        'Login',
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      color: Colors.green,
     );
   }
 }
